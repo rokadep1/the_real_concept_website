5fb07fb4689c698844c1f7e12f97b58f6ddeb6dc
rokadep1 <125141803+rokadep1@users.noreply.github.com>
2025-05-22 08:47:25 +0530

    Create projectData.js

---
+++ images/projects/images/achievements/projectData.js
@@ -0,0 +1,17 @@
+const projectData = [
+  {
+    name: "Living Room Redesign",
+    files: [
+      "images/projects/living-room/photo1.jpg",
+      "images/projects/living-room/photo2.jpg",
+      "images/projects/living-room/video1.mp4"
+    ]
+  },
+  {
+    name: "Kitchen Makeover",
+    files: [
+      "images/projects/kitchen/photo1.jpg",
+      "images/projects/kitchen/video1.mp4"
+    ]
+  }
+];
